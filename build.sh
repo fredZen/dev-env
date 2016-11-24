47eacc7d24a1d69566019c724a778487fd9bcbb3
Frederic Merizen <frederic.merizen@gmail.com>
2016-11-24 11:04:44 +0100

    Let users handle their composing

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd docker
-docker build . -t fmerizen/dev-env
-docker push fmerizen/dev-env
--- dev-env.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd %~dp0
-
-set COMPOSE_PROJECT_NAME=devenv
-
-docker-compose up -d
-docker exec -ti %COMPOSE_PROJECT_NAME%_shell_1 /bin/bash
